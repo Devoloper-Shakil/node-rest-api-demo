2e265d9878e5d79d1c62bd59468187660e72c4db
shakil hasan <shshakilhasan69@gmail.com.com>
2021-10-17 10:56:34 -0700

    add to midelwer

--- api/routes/countactRoutes.js
+++ api/routes/countactRoutes.js
@@ -8,4 +8,15 @@ router.get('/',(req,res,next)=>{
     })
 })
 
+router.post('/',(req,res,next)=>{
+    const name=req.body.name
+    const email=req.body.email
+   
+    res. status(200).json({
+        masege:"hey ,this is my frist data post of datadash",
+        name,
+        email
+    })
+})
+
 module.exports= router
--- server.js
+++ server.js
@@ -1,10 +1,22 @@
-const { application } = require('express')
-const express = require('express')
-const app = express()
-const port = process.env.prot || 5000
-const countactRoutes= require('./api/routes/countactRoutes')
 
-app.use('/api/contact',countactRoutes)
+const express = require('express')
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+var cors = require('cors')
+
+const app = express()
+app.use(morgan('dev'))
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.json())
+app.use(cors())
+
+
+const port = process.env.prot || 5000
+
+const countactRoutes = require('./api/routes/countactRoutes')
+
+
+app.use('/api/contact', countactRoutes)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
